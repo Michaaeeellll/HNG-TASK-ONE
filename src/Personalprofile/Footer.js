16299659d8f77e7a7d39b183696f39274b13629f
Michaaeeellll <115596642+Michaaeeellll@users.noreply.github.com>
2022-10-30 22:33:52 +0100

    added alt

--- src/Personalprofile/Footer.js
+++ src/Personalprofile/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
   return (
     <div>
         <hr id="hr"/>
-        <img src='/images/zuri.svg' id="zurii"/>
+        <img src='/images/zuri.svg' id="zurii" alt="zurii"/>
         <span id="hng">HNG Internship 9 Frontend Task</span>
-        <img src='/images/ingressive.svg' id="ig"/>
+        <img src='/images/ingressive.svg' id="ig" alt="ig"/>
     </div>
   )
 }
