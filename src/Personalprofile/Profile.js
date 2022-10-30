e30dbd33998a7a324f504e892d2194bbe2909a1c
Michaaeeellll <115596642+Michaaeeellll@users.noreply.github.com>
2022-10-30 22:38:42 +0100

    added alt

--- src/Personalprofile/Profile.js
+++ src/Personalprofile/Profile.js
@@ -4,8 +4,8 @@ import './Profile.css';
 export default function Profile() {
   return (
     <div>
-        <img src='/images/FocusShare.svg' id='fs'/>
-        <img src='/images/profile__img.svg' id='profile__img'/>
+        <img src='/images/FocusShare.svg' id='fs' alt='focusshare'/>
+        <img src='/images/profile__img.svg' id='profile__img' alt='profilepic'/>
         <h2 id='twitter'>Akintola Michael</h2>
         {/* <p id='slack'>Michael</p> */}
     </div>
